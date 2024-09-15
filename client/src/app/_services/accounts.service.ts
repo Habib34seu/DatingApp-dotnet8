b0bc87b3ee0525569bd809edb41867b6109df680
Habiseub <34seu.habib@gmail.com>
2024-09-15 22:46:59 +0600

    update

---
+++ client/src/app/_services/accounts.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../_models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  private http = inject(HttpClient);
+  baseurl = 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null);
+
+  login(model: any){
+    return this.http.post<User>(this.baseurl + 'account/login', model).pipe(
+      map(user => {
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
