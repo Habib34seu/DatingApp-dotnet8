ffddc663ae4f69577637729978679b2c919e3969
Habiseub <34seu.habib@gmail.com>
2024-09-20 03:30:55 +0600

    update members

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
+import { MemberCardComponent } from "../member-card/member-card.component";
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [MemberCardComponent],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
-export class MemberListComponent {
+export class MemberListComponent implements OnInit{
+  private memberServices = inject(MembersService);
+  members: Member[]=[];
+  ngOnInit(): void {
+    this.loadMembers();
+  }
 
+  loadMembers(){
+    this.memberServices.getMembers().subscribe({
+      next: members => this.members = members
+    })
+  }
 }
